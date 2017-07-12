75275418a2e4ffaad23119713305db6746ec3238
SENAC\francisco.cnmarao <francisco.cnmarao@sp.senac.br>
2017-07-12 17:45:59 -0300

    login start service

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var db = require('./modules/db');
-
+console.log("starting service [" + new Date().toISOString() + "]");
 //6 hours update
 db.clanUpdate();
 setInterval(function () { db.clanUpdate() }, 1000*60*60*6);//21600000
+console.log("clanUpdate");
 
 //rank update
 db.globalRankUpdate();
 setInterval(function () { db.globalRankUpdate() }, 1000*60*60);
+console.log("globalRankUpdate");
+
+console.log("service started [" + new Date().toISOString() + "]");
