49fce9b4ad7df2559b0354810904c9ebb4c2a992
SENAC\francisco.cnmarao <francisco.cnmarao@sp.senac.br>
2017-07-13 12:57:37 -0300

    showing logs

--- modules/db.js
+++ modules/db.js
@@ -17,15 +17,19 @@ function saveClan(error, obj) {
     //     return;
     // }
     Clan.findOneAndUpdate({ tag: obj.tag }, obj, { upsert: true, new: true, setDefaultsOnInsert: true }, function (err, clan) {
-        if (err)
-            throw err;
+        if (err){
+            console.log(err);
+            return;
+        }
         if (clan) {
             var insert = {
                 tag: obj.tag
             };
             clanHistory.findOneAndUpdate({ tag: insert.tag }, insert, { upsert: true, new: true, setDefaultsOnInsert: true }, function (err, ch) {
-                if (err)
-                    throw err;
+                if (err){
+                    console.log(err);
+                    return;
+                }
                 if (ch) {
                     if (!ch.history)
                         ch.history = [];
@@ -39,8 +43,10 @@ function saveClan(error, obj) {
                         date: new Date()
                     });
                     ch.save(function (error) {
-                        if (error)
-                            throw error;
+                        if (error){
+                            console.log(error);
+                            return;
+                        }
                     });
                 }
             })
@@ -53,8 +59,10 @@ function saveRank(error, response, rnk) {
     //     return;
     // }
     Rank.findOneAndUpdate({ type: rnk.type }, rnk, { upsert: true, new: true, setDefaultsOnInsert: true }, function (err, rank) {
-        if (err)
-            throw err;
+        if (err){
+            console.log(err);
+            return;
+        }
         if (rank.entries.length > 0 && (new Date(rank.entries[rank.entries.length - 1].date)).getDate() != new Date().getDate()) {
             for (var index = 0; index < rank.entries.length; index++) {
                 rank.entries[index].remove();
@@ -69,8 +77,10 @@ function saveRank(error, response, rnk) {
         rank.entries.push(rankEntry);
 
         rank.save(function (error) {
-            if (error)
-                throw error;
+            if (error){
+                console.log(error);
+                return;
+            }
         });
     });
 }
@@ -79,14 +89,18 @@ module.exports =
     {
         clanUpdate: function () {
             Clan.find({}, function (err, clans) {
-                if (err)
-                    throw err;
+                if (err){
+                    console.log(err);
+                    return;
+                }
+                console.log("[" + new Date().toISOString() + "] updating " + clans.length + " clans...");
                 for (var index = 0, clan; clan = clans[index]; index++) {
                     cocRequest.searchClans('clan', clan.tag, saveClan);
                 }
             });
         },
         globalRankUpdate: function () {
+            console.log("[" + new Date().toISOString() + "] globalRankUpdates");
             cocRequest.searchClans('global', 55000, saveRank, { type: "global", date: new Date() });
         }
     }
