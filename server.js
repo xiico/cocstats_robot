3e0add3d18116dbfec0a474a2189894f839c6ca7
SENAC\francisco.cnmarao <francisco.cnmarao@sp.senac.br>
2017-07-13 14:10:07 -0300

    date label log

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var db = require('./modules/db');
-console.log("starting service [" + new Date().toISOString() + "]");
+console.log("[" + new Date().toISOString() + "] service starting...");
 //6 hours update
 db.clanUpdate();
 setInterval(function () { db.clanUpdate() }, 1000*60*60*6);//21600000
-console.log("clanUpdate");
 
 //rank update
 db.globalRankUpdate();
 setInterval(function () { db.globalRankUpdate() }, 1000*60*60);
-console.log("globalRankUpdate");
 
-console.log("service started [" + new Date().toISOString() + "]");
+console.log("[" + new Date().toISOString() + "] service started...");
